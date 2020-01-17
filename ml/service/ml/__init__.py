220239dd9ba0a4946d4e8c61373d6d0ab1dc5483
ducklee89 <ducklee89@email.mail>
2020-01-17 13:47:18 +0900

    commit ML2 papago

--- ml/service/ml/__init__.py
+++ ml/service/ml/__init__.py
@@ -1,59 +1,59 @@
-from sklearn.externals import joblib
-import json
-import re
-
+from sklearn.externals import joblib 
+import json, re 
 # 1. python 3.3 이하 버전과 하위 호환을 위해서 사용
 # 2. 패키지 자체를 지칭할때 사용
 
-PI2=3.144
-
-def a():
-
-    print('구동')
-
 # 0. 경로 -> 상수값 -> 환경변수 혹은 DB에서 획득
-MODEL_PATH = './ml/service/ml/clf_model_202001161419.model'
+MODEL_PATH  = './ml/service/ml/clf_mdoel_202001161419.model'
 MODEL_LABEL = './ml/service/ml/clf_labels.label'
 
-# 1. 모델 로드 (1회만)-> 요청이 많아지던 컨트롤이 가능한지 체크
-clf = joblib.load( MODEL_PATH )
+if __name__ != '__main__':
+    # 1. 모델 로드 (1회만)->요청이 많아지면 컨트롤이 가능한지 체크
+    clf = joblib.load( MODEL_PATH )
+    # 2. 레이블 로드
+    with open(MODEL_LABEL, 'r') as f:
+        clf_label = json.load(f)
 
-# 2. 레이블 로드
-with open(MODEL_LABEL, 'r') as f:
-    clf_label = json.load(f)
-
-
-# 3. 예측 함수 (In: 텍스트, out: 예측결과)
+# 3. 예측 함수 (in:텍스트, out:예측결과)
 def detect_lang( text ):
-    # A. text -> 빈도계산 -----------------------------------------------------------------
-    # text -> 빈도계산 -> 알고리즘에 예측요청(데이터 주입) -> 그 결과를 리턴
-    text = text.lower() # 1. 소문자 처리
-    p = re.compile('[^a-z]') # 2. 정규식(알파벳 소문자만 제외)
-    text = p.sub('' , text) # 소문자만 남는다
-    counts = [0 for n in range(26) ] # 알파벳별 카운트를 담을 공간(리스트)
-    limit_a = ord('a') # 매번 반복해서 작업하니까 그냥 최초 한번 변수로 받아서 사용
-    for word in text:
-        counts[ord(word)-limit_a] += 1 # 문자 1개당 카운트 추가 
-    # 빈도수는 값이 너무 퍼져 있어서 0~1사이로 정규화를 하겠다=>학습효과가 뛰어나니까
-    total_count = sum(counts)
-    freq = list( map( lambda x:x/total_count , counts ) )    #정규화    
+  # A. text -> 빈도계산 ---------------------------------------------------------------
+  text = text.lower()           # 1. 소문자 처리
+  p    = re.compile('[^a-z]')   # 2. 정규식(알파벳소문자만제외)
+  text = p.sub( '' , text )     # 3. 소문자만 남는다
+  counts  = [ 0 for n in range(26) ] # 알파벳별 카운트를 담을 공간(리스트)
+  limit_a = ord('a')            # 매번 반복해서 작업하니까 그냥 최초 한번 변수로 받아서 사용
+  for word in text:
+      counts[ord(word)-limit_a] += 1 # 문자 1개당 카운트 추가   
+  total_count = sum(counts)
+  freq = list( map( lambda x:x/total_count , counts ) )  # 정규화     
+  
+  # B. 알고리즘에 예측요청(데이터 주입) ------------------------------------------------
+  predict = clf.predict( [freq]  )  # 입력 형태를 개발했던 형태와 동일하게 차원을 맞춘다
+  na_code = predict[0]              # 'en' or 'fr' ...
+  na_str  = clf_label[ na_code ]    # '영어', '프랑스어', ...
+  # C. 결과를 리턴 -------------------------------------------------------------------
+  return na_code, na_str
+  #pass
 
-    #  B. 알고리즘 예측요청 (데이터 주입) ------------------------------------------------------
-    predict = clf.predict( [freq] ) # 입력 형태를 개발했던 형태와 동일하게 차원을 맞춘다
-    na_code = predict[0]                      # 'en' or 'fr' ...
-    na_str = clf_label[ na_code ] #'영어', '프랑스어', ...
-    #  C. 결과를 리턴 -------------------------------------------------------------------------
-    return na_code, na_str
-    # pass
+# 개인별로 신청한 키
+CLIENT_ID = 'DG9a4VLy9OS7dxqBvQFO'
+SECRET_KEY = 'WZj8dJt7b2'
 
+'''
+curl "https://openapi.naver.com/v1/papago/n2mt" \
+-H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+-H "X-Naver-Client-Id: DG9a4VLy9OS7dxqBvQFO" \
+-H "X-Naver-Client-Secret: WZj8dJt7b2" \
+-d "source=ko&target=en&text=만나서 반갑습니다." -v
+'''
 # 4. 번역 함수 (현:파파고연동, 향후:RNN 구현)
 def transfer_lang( text, na_input_code='en', na_output_code='ko' ):
-    pass
+    print('파파고와 연동한 번역 처리 시작')
+    return {}
 
-#이 코드는 개발시 테스트 했던 코드이다.
-#의도(개발시)될때만 작동해야 한다.
-
+# 이 코드는 개발시 테스트 했던 코드이다. 
+# 의도(개발시)될때만 작동해야 한다 
 if __name__ == '__main__':
-
-    print('테스트', PI2)
-    a()
+#  print('테스트', PI2)
+  test_str ='Bong Joon-ho (en coréen : 봉준호 prononcé en coréen : /poːŋ tɕuːnho → poːŋdʑunho/), né le 14 septembre 1969 à Daegu, est un réalisateur et scénariste sud-coréen.'
+  transfer_lang( test_str )
--- ml/service/run.py
+++ ml/service/run.py
@@ -3,20 +3,38 @@
 # 플라스크 관련 모듈 가져오기
 from flask import Flask, render_template, request, jsonify, redirect
 # 테스트 모듈 가져오기
-from ml.mod import PI
+from ml.mod import *
 #언어 감지 및 번역 모듈 가져오기
-from ml import detect_lang, transfer_lang
+from ml import detect_lang as dl, transfer_lang
 #1. 모듈 가져오기 end --------------------------------------------------------------------
 
 # 2. Flask 객체 생성
-app = Flask( __name__ )
+app = Flask( __name__ )\
 # 3. 라우팅
 @app.route('/')
 def home():
-    text_str = '''
-...
-'''
-    print( detect_lang(text_str) )
+    import os
+    import sys
+    import urllib.request
+    import ml
+    client_id = ml.CLIENT_ID # "YOUR_CLIENT_ID" # 개발자센터에서 발급받은 Client ID 값
+    client_secret = ml.SECRET_KEY # "YOUR_CLIENT_SECRET" # 개발자센터에서 발급받은 Client Secret 값
+
+    encText = urllib.parse.quote("반갑습니다") # 한글의 URL 인코딩 처리 => %2D..... 변환처리
+    data = "source=ko&target=en&text=" + encText # 파라미터 구성
+    url = "https://openapi.naver.com/v1/papago/n2mt" # 주소
+    request = urllib.request.Request(url) # 요청객체 생성
+    request.add_header("X-Naver-Client-Id",client_id) # 헤더 설정
+    request.add_header("X-Naver-Client-Secret",client_secret) # 헤더 설정
+
+    response = urllib.request.urlopen(request, data=data.encode("utf-8")) # 요청
+    rescode = response.getcode() # 응답코드 획득
+    if(rescode==200): # 응답 성공
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+    else:
+        print("Error Code:" + rescode)
+    
     return render_template('index.html')
 # restful
 @app.route('/bsgo', methods=['GET', 'POST'])
@@ -25,12 +43,39 @@ def bsgo():
         return render_template('bsgo.html')
     else:
         # 전달된 데이터 획득
-        print(request.form['o'])
+        # print(request.form.get['ol'])
+        oriTxt = request.form.get('o') # 내용이 들어있고, 100글자 이상이다.
+        # print(oriTxt)
+        # print(request.form['ol']) # 만약 키가 틀리면 오류를 발생한다.
         # 언어감지
+        na_code, na_str = dl( oriTxt )
+        
+        if na_code:# 예측 되었다
+            res = {
+                'code':na_code,
+                'code_str':na_str
+            }
+        else:
+            res = {
+                'code':'0',
+                'code_str':'언어 감지 실패'
+            }
         # 결과를 응답
-        return ''
+        return jsonify( res )
         # pass
 
+#번역 처리
+@app.route('/transfer', methods=['POST'])
+def transfer():
+    #데이터 획득
+    oriTxt = request.form.get('o')
+    na = request.form.get('na')
+
+    #번역
+    res = transfer_lang( oriTxt, na )
+    #로그 처리
+    #응답
+    return jsonify(res)
 
 # 4. 서버 가동
 if __name__ == '__main__':
