2097b057513fc3cf0775c81e8174e58ac3282055
ducklee89 <ducklee89@email.mail>
2020-01-17 10:54:42 +0900

    commit ML

--- ml/service/ml/__init__.py
+++ ml/service/ml/__init__.py
@@ -1,6 +1,6 @@
 from sklearn.externals import joblib
 import json
-
+import re
 
 # 1. python 3.3 이하 버전과 하위 호환을 위해서 사용
 # 2. 패키지 자체를 지칭할때 사용
@@ -24,9 +24,31 @@ with open(MODEL_LABEL, 'r') as f:
 
 
 # 3. 예측 함수 (In: 텍스트, out: 예측결과)
+def detect_lang( text ):
+    # A. text -> 빈도계산 -----------------------------------------------------------------
+    # text -> 빈도계산 -> 알고리즘에 예측요청(데이터 주입) -> 그 결과를 리턴
+    text = text.lower() # 1. 소문자 처리
+    p = re.compile('[^a-z]') # 2. 정규식(알파벳 소문자만 제외)
+    text = p.sub('' , text) # 소문자만 남는다
+    counts = [0 for n in range(26) ] # 알파벳별 카운트를 담을 공간(리스트)
+    limit_a = ord('a') # 매번 반복해서 작업하니까 그냥 최초 한번 변수로 받아서 사용
+    for word in text:
+        counts[ord(word)-limit_a] += 1 # 문자 1개당 카운트 추가 
+    # 빈도수는 값이 너무 퍼져 있어서 0~1사이로 정규화를 하겠다=>학습효과가 뛰어나니까
+    total_count = sum(counts)
+    freq = list( map( lambda x:x/total_count , counts ) )    #정규화    
+
+    #  B. 알고리즘 예측요청 (데이터 주입) ------------------------------------------------------
+    predict = clf.predict( [freq] ) # 입력 형태를 개발했던 형태와 동일하게 차원을 맞춘다
+    na_code = predict[0]                      # 'en' or 'fr' ...
+    na_str = clf_label[ na_code ] #'영어', '프랑스어', ...
+    #  C. 결과를 리턴 -------------------------------------------------------------------------
+    return na_code, na_str
+    # pass
 
 # 4. 번역 함수 (현:파파고연동, 향후:RNN 구현)
-
+def transfer_lang( text, na_input_code='en', na_output_code='ko' ):
+    pass
 
 #이 코드는 개발시 테스트 했던 코드이다.
 #의도(개발시)될때만 작동해야 한다.
--- ml/service/run.py
+++ ml/service/run.py
@@ -1,15 +1,36 @@
 # 엔트리 포인트 : 진입로, 시작점, 모든 경로법은 엔트리로부터 따진다.
-# 1. 모듈 가져오기
+# 1. 모듈 가져오기 ----------------------------------------------------------------------
+# 플라스크 관련 모듈 가져오기
 from flask import Flask, render_template, request, jsonify, redirect
-from ml.mod import *
-from ml import PI2
+# 테스트 모듈 가져오기
+from ml.mod import PI
+#언어 감지 및 번역 모듈 가져오기
+from ml import detect_lang, transfer_lang
+#1. 모듈 가져오기 end --------------------------------------------------------------------
+
 # 2. Flask 객체 생성
 app = Flask( __name__ )
 # 3. 라우팅
 @app.route('/')
 def home():
-    print( '-', PI, PI2 )
+    text_str = '''
+...
+'''
+    print( detect_lang(text_str) )
     return render_template('index.html')
+# restful
+@app.route('/bsgo', methods=['GET', 'POST'])
+def bsgo():
+    if request.method == 'GET':
+        return render_template('bsgo.html')
+    else:
+        # 전달된 데이터 획득
+        print(request.form['o'])
+        # 언어감지
+        # 결과를 응답
+        return ''
+        # pass
+
 
 # 4. 서버 가동
 if __name__ == '__main__':
